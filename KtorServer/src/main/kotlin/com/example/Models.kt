44263c24c21bfa4f5251bad024480eb56142e575
gloriadukuzeyesu <gloriadukuzeyesu@gmail.com>
2023-11-01 00:05:35 -0500

    adding ktor server.

---
+++ KtorServer/src/main/kotlin/com/example/Models.kt
@@ -0,0 +1,12 @@
+package com.example
+
+import org.jetbrains.exposed.dao.id.IntIdTable
+// tables for our db
+//  Table  of paints
+object PaintTable : IntIdTable() {
+    val userID = varchar("userID", 255)
+    val imagePath = varchar("imagePath", 255)
+    val title = varchar("title", 255)
+    val timeStamp = long("timeStamp")
+}
+
---
+++ customViewDemo/app/src/main/java/com/example/customviewdemo/network/ServerViewModel.kt
@@ -0,0 +1,4 @@
+package com.example.customviewdemo.Network
+
+class ServerViewModel {
+}
